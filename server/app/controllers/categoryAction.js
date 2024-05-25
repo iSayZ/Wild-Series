173e9ccd7a50e5e350baefa6fd98c5bce8b2274b
iSayZ <estrine.alexis@gmail.com>
2024-05-25 13:33:42 +0200

    Adding CategoryRepository and ProgramRepository for acces to DB

--- server/app/controllers/categoryAction.js
+++ server/app/controllers/categoryAction.js
@@ -1,29 +1,37 @@
+const tables = require("../../database/tables");
+
 // Some data to make the trick
 
-const categories = [
-    {
-      id: 1,
-      name: "Science-Fiction",
-    },
-    {
-      id: 2,
-      name: "Comédie",
-    },
-  ];
+// const categories = [
+//     {
+//       id: 1,
+//       name: "Science-Fiction",
+//     },
+//     {
+//       id: 2,
+//       name: "Comédie",
+//     },
+//   ];
   
   // Declare the actions
   
-  const browse = (req, res) => {
-    if (req.query.cat != null) {
-      const filteredCategories = categories.filter((cat) =>
-        cat.name.includes(req.query.cat)
-      );
+  // const browse = (req, res) => {
+  //   if (req.query.cat != null) {
+  //     const filteredCategories = categories.filter((cat) =>
+  //       cat.name.includes(req.query.cat)
+  //     );
   
-      res.json(filteredCategories);
-    } else {
-      res.json(categories);
-    }
+  //     res.json(filteredCategories);
+  //   } else {
+  //     res.json(categories);
+  //   }
+  // };
+ 
+  const browse = async (req, res) => {
+    const categoriesFromDB = await tables.category.readAll();
+    res.json(categoriesFromDB);
   };
+    
   
   const read = (req, res) => {
     const parsedId = parseInt(req.params.id, 10);
